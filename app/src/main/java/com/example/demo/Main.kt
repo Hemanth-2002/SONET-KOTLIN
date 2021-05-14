5ef0748c8c1399c4d2639b7db71ffc52f6bb2b1d
Hemanth-2002 <hemanthkakumanu1@gmail.com>
2021-05-14 14:18:09 +0530

    app2_commit

--- app/src/main/java/com/example/demo/Main.kt
+++ app/src/main/java/com/example/demo/Main.kt
@@ -1,13 +1,11 @@
 package com.example.demo
 
 fun main() {
-    var myInt = 25
-    var myFloat:Float =13.37F
-    var s="Android Kotlin"
-    var myDouble:Double =3.1415926535979
-    print(s+"\n")
-    print("$myFloat"+"\n")
-    print("$myDouble"+"\n")
-    print("$myInt"+"\n")
-
+    var a = arrayOf<Int>(1,2,3)
+    var b = 2
+    var c= b.toString()
+    var i:Int
+    for(i in 1..5){
+        print("$i ")
+    }
 }
