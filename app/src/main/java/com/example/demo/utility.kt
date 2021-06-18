8f9b5d79124112366eb9e22221413a51eba04210
Hemanth-2002 <hemanthkakumanu1@gmail.com>
2021-06-18 21:21:17 +0530

    Quiz 3

--- app/src/main/java/com/example/demo/utility.kt
+++ app/src/main/java/com/example/demo/utility.kt
@@ -1,11 +1,11 @@
 package com.example.demo
 
-class utility {
+class Utility {
+
     fun getQuestion():ArrayList<Questions>{
         var list = ArrayList<Questions>()
-        //logic
-        var q1 = Questions(1,"What is kotlin?","Scripting Language","Programming Language","Modern Language","Assembly Language")
-        var q2 = Questions(2,"What is Java?","Scripting Language","Programming Language","Modern Language","Assembly Language")
+        var q1 = Questions(1,"What is kotlin?","Scripting Language","Programming Language","Modern Language","Assembly Language",1)
+        var q2 = Questions(2,"What is Java?","Scripting Language","Programming Language","Modern Language","Assembly Language",2)
         list.add(q1);
         list.add(q2)
 
