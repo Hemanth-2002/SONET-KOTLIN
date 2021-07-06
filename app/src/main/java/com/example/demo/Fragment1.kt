f6a00088f43f8adca564439b10e0ed043ecd42c8
Hemanth-2002 <hemanthkakumanu1@gmail.com>
2021-07-06 21:30:43 +0530

    fragment

---
+++ app/src/main/java/com/example/demo/Fragment1.kt
@@ -0,0 +1,28 @@
+package com.example.demo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.demo.databinding.Fragment1Binding
+
+class Fragment1 : Fragment() {
+    //logic
+    lateinit var binder : Fragment1Binding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binder = Fragment1Binding.inflate(layoutInflater)
+        return binder.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binder.fragmentChangeBtn.setOnClickListener(View.OnClickListener {
+            var fragmentManager = activity?.supportFragmentManager
+            var fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.replace(R.id.demo_fragment_container, Fragment2())
+            fragmentTransaction?.commit()
+        })
+    }
+}
